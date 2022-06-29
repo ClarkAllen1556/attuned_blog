a43a8fa8a6813c8e8067e6c09f395cb9434da82c
AC1556 <allen@urbanlife.me>
2022-06-29 11:22:39 +0900

    better styling for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,9 @@ import { ReactNode } from 'react';
 
 export default function Header({ children }: { children?: ReactNode }) {
   return (
-    <header className="h-20 w-full inline-flex">
-      <div className="inline-flex items-center gap-2">
-        <h1 className="text-sol-black-1"> ~$ </h1>
-        <h1> UrbanBlog </h1>
-      </div>
+    <header className="inline-flex items-center justify-center lg:justify-start h-20 w-full">
+      <h1 className="text-sol-black-1 mr-2"> ~$ </h1>
+      <h1> UrbanBlog </h1>
 
       <div>{children}</div>
     </header>
