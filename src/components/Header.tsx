73e447e6a8ea92e3d8df363ae1895c62c762e4e8
AC1556 <allen@urbanlife.me>
2022-06-29 17:20:42 +0900

    Refactoring stylings for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from 'react';
 
 export default function Header({ children }: { children?: ReactNode }) {
   return (
-    <header className="inline-flex items-center justify-center lg:justify-start h-20 w-full">
-      <h1 className="text-sol-black-1 mr-2"> ~$ </h1>
-      <h1> UrbanBlog </h1>
+    <header className="flex flex-col lg:flex-row items-center justify-center lg:justify-between h-20 w-full">
+      <h1>
+        <span className="text-sol-black-1 mr-2">~$</span>UrbanBlog
+      </h1>
 
-      <div>{children}</div>
+      <div className="">{children}</div>
     </header>
   );
 }
