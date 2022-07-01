15b4542b68ec689a1567899c2b3fa73fdef486ed
AC1556 <allen@urbanlife.me>
2022-07-01 18:25:37 +0900

    adding functional search

--- src/features/feed/feed.ts
+++ src/features/feed/feed.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchPosts } from '~/common/api/posts.api';
+import { fetchPosts, searchPosts } from '~/common/api/posts.api';
 
 import { IPost } from '~/common/interfaces/Post.interface';
 import { IRequest } from '~/common/interfaces/Request.interface';
@@ -29,13 +29,26 @@ export const feedSlice = createSlice({
     builder.addCase(populateFeed.fulfilled, (state, action) => {
       state.posts = action.payload;
     });
+
+    builder.addCase(searchFeed.fulfilled, (state, action) => {
+      state.posts = action.payload;
+    });
   },
 });
 
 export const populateFeed = createAsyncThunk(
   'feed/populateFeed',
-  async ({ page, limit }: IRequest) => {
-    const posts = await fetchPosts({ page, limit });
+  async (opts: IRequest) => {
+    const posts = await fetchPosts(opts);
+
+    return posts;
+  }
+);
+
+export const searchFeed = createAsyncThunk(
+  'feed/searchFeed',
+  async (query: string | null) => {
+    const posts = await searchPosts(query);
 
     return posts;
   }
