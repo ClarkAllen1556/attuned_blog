c5bb09bd16c873c54fb7bb52c0b77c55ad744608
AC1556 <allen@urbanlife.me>
2022-07-01 14:31:55 +0900

    clicking on show comment button fetches commetns

--- src/features/feed/Feed.tsx
+++ src/features/feed/Feed.tsx
@@ -1,29 +1,30 @@
+import { useAppDispatch, useAppSelector } from '~/common/hooks';
 import { IPost } from '~/common/interfaces/Post.interface';
 import Button from '~/components/Button';
 import Card from '~/components/Card';
 import Post from '~/components/Post';
+import { populateComments } from '../comment/comment';
 
 interface Props {
   posts: IPost[];
 }
 
 function Feed({ posts }: Props) {
+  const comments = useAppSelector((state) => state.comments);
+  const dispatch = useAppDispatch();
+
   const showComments = (postId: number) => {
-    console.log(posts.find((p) => p.id === postId));
+    dispatch(populateComments({ postId: postId }));
   };
 
   function showCommentsButton(post: IPost) {
-    // if (post.comments.length) {
-    //   return (
-    //     <Button
-    //       variant="text"
-    //       btnLabel={`Show comments (${post.comments.length})`}
-    //       click={() => showComments(post.id)}
-    //     />
-    //   );
-    // }
-
-    return <em> No comments </em>;
+    return (
+      <Button
+        variant="text"
+        btnLabel={`Show comments `}
+        click={() => showComments(post.id)}
+      />
+    );
   }
   return (
     <div className="w-full p-8 max-w-5xl ml-auto mr-auto">
